0afa7dac8683cdfcf54ceac85c557d544a37596b
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-12-19 21:42:13 -0600

    added review page

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -1,9 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Reviews from "../components/Reviews";
+ 
 const Review = () => {
-    return `
-    `
-};
 
+    return (
+        <div>
+        <h1>Leave a review today!</h1>
+            <Reviews/>
+            <section></section>
+
+        </div>
+
+    );
+};
 
 export default Review;
