a035a8dafb508ff8bc32d0b7fd28d654016c9341
Tyler Mason <109122403+JayWhiteBuffalo@users.noreply.github.com>
2022-12-20 18:30:24 -0600

    Update seeds.js
    Uncommented Seed Data

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -9,24 +9,24 @@ db.once('open', async () => {
   await Room.deleteMany({});
   await Reservation.deleteMany({});
 
-  // // create user data
-  // const userData = [];
+   // create user data
+     const userData = [];
 
-  // for (let i = 0; i < 50; i += 1) {
-  //   const username = faker.internet.userName();
-  //   const email = faker.internet.email(username);
-  //   const password = faker.internet.password();
+     for (let i = 0; i < 50; i += 1) {
+       const username = faker.internet.userName();
+       const email = faker.internet.email(username);
+       const password = faker.internet.password();
 
-  //   userData.push({ username, email, password });
-  // }
+       userData.push({ username, email, password });
+    }
 
-  // const createdUsers = await User.collection.insertMany(userData);
+    const createdUsers = await User.collection.insertMany(userData);
 
-  // // create reviews
-  // let createdReviews = [];
-  // for (let i = 0; i < 100; i += 1) {
-  //   const reviewText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-  //   const reviewStars = 5;
+      create reviews
+    let createdReviews = [];
+    for (let i = 0; i < 100; i += 1) {
+    const reviewText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const reviewStars = 5;
 
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
     const { _id: userId } = createdUsers.ops[randomUserIndex];
@@ -34,16 +34,16 @@ db.once('open', async () => {
 
     const createdReview = await Review.create({ reviewText, reviewStars, user });
 
-  //   const updatedUser = await User.updateOne(
-  //     { _id: userId },
-  //     { $push: { reviews: createdReview._id } }
-  //   );
+     const updatedUser = await User.updateOne(
+       { _id: userId },
+       { $push: { reviews: createdReview._id } }
+     );
 
     createdReviews.push(createdReview);
   }
 
   // // create room data
-  // const roomData = [];
+   const roomData = [];
 
   for (let i = 0; i < 10; i += 1) {
     const roomType = faker.random.word();
@@ -54,26 +54,26 @@ db.once('open', async () => {
   }
   const createdRooms = await Room.collection.insertMany(roomData);
 
-  // // create reservations
-  // let createdReservations = [];
-  // for (let i = 0; i < 10; i += 1) {
-  //   const arrivalDate = faker.date.past();
-  //   const departureDate = faker.date.future();
+   // create reservations
+   let createdReservations = [];
+   for (let i = 0; i < 10; i += 1) {
+     const arrivalDate = faker.date.past();
+     const departureDate = faker.date.future();
   
-  //   const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-  //   const { _id: userId } = createdUsers.ops[randomUserIndex];
-  //   const user = userId
+     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+     const { _id: userId } = createdUsers.ops[randomUserIndex];
+     const user = userId
 
-  //   const randomRoomTypeIndex = Math.floor(Math.random() * createdRooms.ops.length);
-  //   const { _id: roomId } = createdRooms.ops[randomRoomTypeIndex];
-  //   const room = roomId
+     const randomRoomTypeIndex = Math.floor(Math.random() * createdRooms.ops.length);
+     const { _id: roomId } = createdRooms.ops[randomRoomTypeIndex];
+     const room = roomId
 
-  //   const createdReservation = await Reservation.create({ arrivalDate, departureDate, user, room });
+     const createdReservation = await Reservation.create({ arrivalDate, departureDate, user, room });
 
-  //   const updatedUser = await User.updateOne(
-  //     { _id: userId },
-  //     { $push: { reservations: createdReservation._id } }
-  //   );
+     const updatedUser = await User.updateOne(
+       { _id: userId },
+       { $push: { reservations: createdReservation._id } }
+     );
 
     createdReservations.push(createdReservation);
   }
