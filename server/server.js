9d7369a1584d5243e9b22c65842479e30b08f033
White Buffalo <apothocare@gmail.com>
2022-12-23 06:02:19 -0600

    Modified files for deployment

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
