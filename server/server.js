1689d44bb594cd6a277ac1c7b6bd0d4757a4dec4
tracybrtn <tracynburton@gmail.com>
2022-12-22 22:36:32 -0600

    local changes

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
+  app.get("/*", (req, res) {
       res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
 }
