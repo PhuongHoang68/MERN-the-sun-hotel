5276825a68411651b24fe8634e5bc3888373f507
White Buffalo <apothocare@gmail.com>
2023-01-03 02:44:46 -0600

    Added Pop-up Calander for Book section on landing page. Modified styles on Calendar

--- client/src/components/BookNowBar/index.js
+++ client/src/components/BookNowBar/index.js
@@ -8,6 +8,16 @@ import 'react-calendar/dist/Calendar.css';
 
 const BookNowBar = () => {
 
+const [calendarActive, setCalendarActive] = useState(false);
+
+//tracks Calander date input
+const [date, setDate] = useState((new Date()));
+
+const toggleCalendar = () =>{
+    setCalendarActive(!calendarActive)
+    console.log(calendarActive)
+}
+
 
 return (
 
@@ -16,13 +26,17 @@ return (
             <div className="bnArrival">
                 <div>
                 <FaCalendar/>
-                <input  type="text"></input>
+                <input  type="text" placeholder="Arrive" onFocus={toggleCalendar}></input>
                 </div>
                 <div>
                 <FaCalendar/>
-                <input  type="text"></input>
+                <input  type="text" placeholder="Depart" onFocus={toggleCalendar}></input>
                 </div>
             </div>
+            {calendarActive === true ? (<div>
+                <Calendar
+                    id = "Calendar"/>
+                    </div>) : ( <div>
             <div className="bnRooms">
             <select name="rooms" id="rooms">
                 <option value="undefined">Choose Room Type</option>
@@ -32,6 +46,7 @@ return (
               </select>
             </div>
             <button> Check available</button>
+           </div> ) }
         </div>
     </div>
 
